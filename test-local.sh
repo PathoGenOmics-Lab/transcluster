96f4edfe6bbc5216e28ba61445a68a44de8ad8b3
Miguel <ahmig@users.noreply.github.com>
2023-02-13 19:00:53 +0100

    Update examples

--- test-local.sh
+++ test-local.sh
@@ -2,7 +2,7 @@
 
 snakemake \
     --use-conda \
-    -c 4 \
+    -c 2 \
     --config ALIGNED_FULL_FASTA="test/full_fasta.fasta" \
              INPUT_DIR="test/input" \
              OUTPUT_DIR="test/output"
