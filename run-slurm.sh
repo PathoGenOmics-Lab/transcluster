2f58ffeb042a94043be151db73253ef7ec176ce2
Miguel Álvarez Herrera <m.alvarez.herrera@csic.es>
2023-02-17 11:41:37 +0100

    Update naming of slurm jobs

--- run-slurm.sh
+++ run-slurm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH -J tc-main
+#SBATCH -J tc
 #SBATCH --part global
 #SBATCH --qos medium
 #SBATCH -t 7-00:00:00
@@ -13,5 +13,5 @@ SLURM_CONFIG="sm/slurm.yaml"
 snakemake \
     --use-conda \
     --cluster-config "$SLURM_CONFIG" \
-    --cluster "sbatch -J {cluster.name} -t {cluster.time} --qos {cluster.qos} -N {cluster.nodes} --cpus-per-task {cluster.cpus} --mem {cluster.mem} --output {cluster.out}" \
+    --cluster "sbatch -J $SLURM_JOB_NAME-{cluster.name} -t {cluster.time} --qos {cluster.qos} -N {cluster.nodes} --cpus-per-task {cluster.cpus} --mem {cluster.mem} --output {cluster.out}" \
     --jobs $MAXJOBS
