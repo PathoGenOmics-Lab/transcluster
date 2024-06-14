522f98ee3b64f4596924a71fe2906ad641502e2c
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-06-14 13:28:19 +0200

    Update launcher script

--- run-slurm.sh
+++ run-slurm.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -J tc
 #SBATCH --part global
-#SBATCH --qos long
-#SBATCH -t 15-00:00:00
+#SBATCH --qos medium
+#SBATCH -t 7-00:00:00
 #SBATCH --mem 4GB
 #SBATCH -c 1
 #SBATCH -o slurm-transcluster-%j_main.out
@@ -12,4 +12,4 @@ SHADOW="/scr/$USER"
 snakemake \
     --use-conda \
     --shadow-prefix "$SHADOW" \
-    --workflow-profile profiles/default/garnatxa
+    --workflow-profile profiles/garnatxa
