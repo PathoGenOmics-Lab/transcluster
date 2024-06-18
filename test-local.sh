1cc1920baac37046cbd8befb853c891905597ff9
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-06-18 13:55:12 +0200

    Fix test alignment

--- test-local.sh
+++ test-local.sh
@@ -3,7 +3,7 @@
 snakemake \
     --use-conda \
     -c 2 \
-    --config ALIGNED_FULL_FASTA="test/full_fasta.fasta" \
+    --config ALIGNED_FULL_FASTA="test/full_fasta.aln.fasta" \
              INPUT_DIR="test/input" \
              OUTPUT_DIR="test/output" \
              METADATA="test/metadata.tsv"
