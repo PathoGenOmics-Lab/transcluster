c69ebcbb31627c0bdaa3e9de5274c4c192ebbef8
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-06-19 15:02:37 +0200

    Add timeline summary report

---
+++ workflow/scripts/report_summarized_timeline.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(logger)
+library(ggpubr)
+
+Sys.setlocale("LC_TIME", "English")
+theme_set(theme_pubclean())
+
+datecol <- sym(snakemake@params[["metadata_date_column"]])
+
+log_info("Reading plot data")
+plot.data <- lapply(
+        snakemake@input[["plot_data_tables"]],
+        read_csv
+    ) %>%
+    bind_rows
+
+log_info("Writing report")
+ggplot(plot.data, aes(x = !!datecol, y = Haplotype)) +
+    geom_point(
+        aes(color = Country),
+        size = 1,
+        alpha = 0.5,
+        position = position_dodge(width = -0.75)
+    ) +
+    geom_violin(linewidth = 0, fill = "black", alpha = 0.4) +
+    scale_x_date(
+        date_breaks = "3 month",
+        date_minor_breaks = "1 month",
+        date_labels = "%b %Y") +
+    scale_color_viridis_d()
+
+ggsave(
+    snakemake@output[["report"]],
+    width = snakemake@params[["width_mm"]],
+    height = length(plot.data) * snakemake@params[["height_per_haplotype_mm"]],
+    units = "mm"
+)
+
+log_info("Writing report data")
+plot.data %>% write_csv(snakemake@output[["report_data"]])
