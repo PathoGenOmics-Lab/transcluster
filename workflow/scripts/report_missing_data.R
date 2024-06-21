8f37a3d09b35c440adf58417289e26109ae41e07
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-06-21 10:46:56 +0200

    Add report of missing values

---
+++ workflow/scripts/report_missing_data.R
@@ -0,0 +1,77 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(logger)
+
+Sys.setlocale("LC_TIME", "English")
+theme_set(theme_classic() + scale_fill_viridis_d(begin = 0.2, end = 0.8))
+
+agecol <- sym(snakemake@params[["metadata_age_column"]])
+datecol <- sym(snakemake@params[["metadata_date_column"]])
+loccol <- sym(snakemake@params[["metadata_location_column"]])
+
+
+log_info("Reading age-corrected metadata")
+metadata <- lapply(
+    snakemake@input[["fitnesses"]],
+    function(path) {
+        read_csv(
+            path,
+            col_select = c(!!agecol, !!datecol),
+            col_types = cols(!!agecol := "n", !!datecol := "D")
+        )
+    }
+) %>% bind_rows
+
+
+log_info("Plotting missing ages")
+ggplot(metadata, aes(Haplotype, fill = is.na(!!agecol))) +
+    geom_bar(stat = "count") +
+    geom_text(
+        aes(label = ..count..),
+        stat = "count",
+        position = position_stack(vjust = 0.5)
+    ) +
+    ggtitle("Missing age values")
+
+ggsave(
+  snakemake@output[["report_age"]],
+  width = snakemake@params[["width_mm"]],
+  height = snakemake@params[["height_mm"]],
+  units = "mm"
+)
+
+
+log_info("Plotting missing locations")
+ggplot(metadata, aes(Haplotype, fill = is.na(!!loccol))) +
+    geom_bar(stat = "count") +
+    geom_text(
+        aes(label = ..count..),
+        stat = "count",
+        position = position_stack(vjust = 0.5)
+    ) +
+    ggtitle("Missing location values")
+
+ggsave(
+  snakemake@output[["report_location"]],
+  width = snakemake@params[["width_mm"]],
+  height = snakemake@params[["height_mm"]],
+  units = "mm"
+)
+
+log_info("Plotting missing dates")
+ggplot(metadata, aes(Haplotype, fill = is.na(!!datecol))) +
+    geom_bar(stat = "count") +
+    geom_text(
+        aes(label = ..count..),
+        stat = "count",
+        position = position_stack(vjust = 0.5)
+    ) +
+    ggtitle("Missing date values")
+
+ggsave(
+  snakemake@output[["report_date"]],
+  width = snakemake@params[["width_mm"]],
+  height = snakemake@params[["height_mm"]],
+  units = "mm"
+)
